d349c185cad2256333292c308e3a62940aee9980
vishal singhi <v.singhi2107@gmail.com>
2023-03-17 15:07:12 +0530

    description

--- Emart/app/src/main/java/com/example/emart/electronicproductcard.java
+++ Emart/app/src/main/java/com/example/emart/electronicproductcard.java
@@ -9,11 +9,12 @@ import android.os.Bundle;
 import java.util.ArrayList;
 
 import adapter.E_productadapter;
+import adapter.F_productadapter;
 import models.E_productitem;
 
 public class electronicproductcard extends AppCompatActivity {
 
-    ArrayList<E_productitem>E_prductlist;
+    ArrayList<E_productitem> arrayList = new ArrayList<>();
     E_productadapter adapater;
     RecyclerView E_playout;
 
@@ -22,19 +23,15 @@ public class electronicproductcard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electronicproductcard);
         E_playout = findViewById(R.id.electronicproductcard);
-
-        E_prductlist = new ArrayList<>();
-        E_prductlist.add(new E_productitem(R.drawable.ele1,1,"Macbook pro 2022","20 rating","80000/-"));
-        E_prductlist.add(new E_productitem(R.drawable.ele2,2,"Lenovo ideapad flex","10 rating","96000/-"));
-        E_prductlist.add(new E_productitem(R.drawable.ele3,3,"Iphone 14 pro max","15 rating","127000/-"));
-        E_prductlist.add(new E_productitem(R.drawable.ele4,4,"Boult z40 earpods ","30 rating","1500/-"));
-        E_prductlist.add(new E_productitem(R.drawable.ele5,5,"Apple i-watch pro 22","10 rating","30000/-"));
-
-
-
-        adapater = new E_productadapter(E_prductlist,this);
-        E_playout.setAdapter(adapater);
         E_playout.setLayoutManager(new GridLayoutManager(this,2));
 
+        arrayList.add(new E_productitem(R.drawable.ele1,1,"Macbook pro 2022","20 rating","80000/-"));
+        arrayList.add(new E_productitem(R.drawable.ele2,2,"Lenovo ideapad flex","10 rating","96000/-"));
+        arrayList.add(new E_productitem(R.drawable.ele3,3,"Iphone 14 pro max","15 rating","127000/-"));
+        arrayList.add(new E_productitem(R.drawable.ele4,4,"Boult z40 earpods ","30 rating","1500/-"));
+        arrayList.add(new E_productitem(R.drawable.ele5,5,"Applxe i-watch pro 22","10 rating","30000/-"));
+
+        E_productadapter e_productadapter = new E_productadapter(this,arrayList);
+        E_playout.setAdapter(e_productadapter);
     }
 }
--- Emart/app/src/main/java/com/example/emart/furnitureproductcard.java
+++ Emart/app/src/main/java/com/example/emart/furnitureproductcard.java
@@ -1,10 +1,11 @@
 package com.example.emart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import java.util.ArrayList;
@@ -13,27 +14,28 @@ import adapter.F_productadapter;
 import models.F_productitem;
 
 public class furnitureproductcard extends AppCompatActivity {
-ArrayList<F_productitem>F_productlist;
+ArrayList<F_productitem> arrayList = new ArrayList<>();
 F_productadapter adapater;
 RecyclerView F_playout;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_furnitureproductcard);
         F_playout = findViewById(R.id.furnitureproductcard);
-
-        F_productlist = new ArrayList<>();
-        F_productlist.add(new F_productitem(R.drawable.fur1,1,"Classic Tethered Metallic Table","13 rating","15000/-"));
-        F_productlist.add(new F_productitem(R.drawable.fur5,2,"White furnished Shoe rack","10 rating","20000/-"));
-        F_productlist.add(new F_productitem(R.drawable.fur3,3,"Aesthetic blush fur sofa","5 rating","25000/-"));
-        F_productlist.add(new F_productitem(R.drawable.fur4,4,"FullFurnished Dining Table","17 rating","60000/-"));
-        F_productlist.add(new F_productitem(R.drawable.f2,5,"Grey couch single sofa","13 rating","22000/-"));
-
-
-
-        adapater = new F_productadapter(F_productlist,this);
-        F_playout.setAdapter(adapater);
         F_playout.setLayoutManager(new GridLayoutManager(this,2));
 
+        arrayList.add(new F_productitem(R.drawable.fur1,1,"Classic Tethered Metallic Table","13 rating","15000/-"));
+        arrayList.add(new F_productitem(R.drawable.fur5,2,"White furnished Shoe rack","10 rating","20000/-"));
+        arrayList.add(new F_productitem(R.drawable.fur3,3,"Aesthetic blush fur sofa","5 rating","25000/-"));
+        arrayList.add(new F_productitem(R.drawable.fur4,4,"FullFurnished Dining Table","17 rating","60000/-"));
+        arrayList.add(new F_productitem(R.drawable.f2,5,"Grey couch single sofa","13 rating","22000/-"));
+
+        F_productadapter f_productadapter = new F_productadapter(this,arrayList);
+        F_playout.setAdapter(f_productadapter);
+
     }
+
+
 }
